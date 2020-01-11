8fa27f8a5af844ae192c37eb564a7c3b0447e717
svetoslav dekov <svetoslav.dekov@mentormate.com>
2020-01-11 17:33:46 +0200

    Repeat last log record

--- static/js/index.jsx
+++ static/js/index.jsx
@@ -37,84 +37,114 @@ class LogRecord extends React.Component{
             weight: '',
             timeLength: '',
             notes: '',
-            isWarmup: false
+            isWarmup: false,
+            lastAdded: {}
         };
     }
 
     onSubmit() {
+        this.createLogRecord(this.state);
+    }
+
+    createLogRecord(data) {
         return fetch('/api/log-records/', {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                name: this.state.excercise,
-                reps: this.state.reps,
-                weight: this.state.weight,
-                time_length: this.state.timeLength,
-                notes: this.state.notes,
-                is_warmup: this.state.isWarmup
-            })
+            body: this._prepareData(data)
         }).then((resp) => {
-            alert(resp.status);
+            if (resp.status.toString()[0] !== '2') {
+                alert(resp);
+                return;
+            }
             this.setState({
                 excercise: '',
                 reps: '',
                 weight: '',
                 timeLength: '',
                 notes: '',
-                isWarmup: false
+                isWarmup: false,
+                lastAdded: data
             });
+        });
+    }
+
+    onRepeatLast() {
+        this.createLogRecord(this.state.lastAdded);
+    }
+
+    _prepareData(data) {
+        return JSON.stringify({
+            name: data.excercise,
+            reps: data.reps,
+            weight: data.weight,
+            time_length: data.timeLength,
+            notes: data.notes,
+            is_warmup: data.isWarmup
         });
     }
 
     render() {
         return (
-            <div>
-                <ReactAutocomplete
-                    getItemValue={(item) => item.name}
-                    items={this.props.excercises}
-                    renderItem={(item, isHighlighted) =>
-                        <div style={{ background: isHighlighted ? 'lightgray' : 'white' }} key={item.name}>
-                            {item.name}
-                        </div>
-                    }
-                    value={this.state.excercise}
-                    onChange={(e) => this.setState({'excercise': e.target.value})}
-                    inputProps={{placeHolder: "excercise"}}
-                    shouldItemRender={(item, value) => item.name.toLowerCase().indexOf(value.toLowerCase()) > -1}
-                    onSelect={(val) => this.setState({'excercise': val})}
-                />
-                <div>
-                    <input type="number"
-                           value={this.state.reps}
-                           onChange={e => this.setState({'reps': e.target.value})}
-                           placeholder='reps'/>
+            <div className="flex-block">
+                <div className="create-log-record">
+                    <ReactAutocomplete
+                        getItemValue={(item) => item.name}
+                        items={this.props.excercises}
+                        renderItem={(item, isHighlighted) =>
+                            <div style={{ background: isHighlighted ? 'lightgray' : 'white' }} key={item.name}>
+                                {item.name}
+                            </div>
+                        }
+                        value={this.state.excercise}
+                        onChange={(e) => this.setState({'excercise': e.target.value})}
+                        inputProps={{placeHolder: "excercise"}}
+                        shouldItemRender={(item, value) => item.name.toLowerCase().indexOf(value.toLowerCase()) > -1}
+                        onSelect={(val) => this.setState({'excercise': val})}
+                    />
+                    <div>
+                        <input type="number"
+                            value={this.state.reps}
+                            onChange={e => this.setState({'reps': e.target.value})}
+                            placeholder='reps'/>
+                    </div>
+                    <div>
+                        <input type="number"
+                            value={this.state.weight}
+                            onChange={e => this.setState({'weight': e.target.value})}
+                            placeholder='weight'/>
+                    </div>
+                    <div>
+                        <input type="number"
+                            value={this.state.timeLength}
+                            onChange={e => this.setState({'timeLength': e.target.value})}
+                            placeholder='time length seconds'/>
+                    </div>
+                    <div>
+                        <input type="number"
+                            value={this.state.notes}
+                            onChange={e => this.setState({'notes': e.target.value})}
+                            placeholder='notes'/>
+                    </div>
+                    <div>
+                        <label>Warmup?</label>
+                        <input type="checkbox"
+                            checked={this.state.isWarmup}
+                            onChange={e => this.setState({'isWarmup': e.target.checked})}
+                            />
+                    </div>
+                    <button onClick={this.onSubmit.bind(this)}>submit</button>
+                    <button onClick={this.onRepeatLast.bind(this)}>repeat last</button>
                 </div>
+                {(() => Object.entries(this.state.lastAdded).length === 0 ? '' : (
                 <div>
-                    <input type="number"
-                           value={this.state.weight}
-                           onChange={e => this.setState({'weight': e.target.value})}
-                           placeholder='weight'/>
+                    <h4>Last Added</h4>
+                    <p>Excercise: {this.state.lastAdded.excercise}</p>
+                    <p>Reps: {this.state.lastAdded.reps}</p>
+                    <p>Weight: {this.state.lastAdded.weight} kg</p>
+                    <p>Time Length: {this.state.lastAdded.timeLength}</p>
+                    <p>Notes: {this.state.lastAdded.notes}</p>
                 </div>
-                <div>
-                    <input type="number"
-                           value={this.state.timeLength}
-                           onChange={e => this.setState({'timeLength': e.target.value})}
-                           placeholder='time length seconds'/>
-                </div>
-                <div>
-                    <input type="number"
-                           value={this.state.notes}
-                           onChange={e => this.setState({'notes': e.target.value})}
-                           placeholder='notes'/>
-                </div>
-                <div>
-                    <label>Warmup?</label>
-                    <input type="checkbox"
-                           checked={this.state.isWarmup}
-                           onChange={e => this.setState({'isWarmup': e.target.checked})}
-                           />
-                </div>
-                <button onClick={this.onSubmit.bind(this)}>submit</button>
+                ))()}
             </div>
         )
     }
