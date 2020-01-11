f06acd5c48dcafe3a16429cce220780ecede7e7e
svetoslav dekov <svetoslav.dekov@mentormate.com>
2020-01-11 18:59:02 +0200

    Require login for home page

--- training_logger/urls.py
+++ training_logger/urls.py
@@ -1,10 +1,22 @@
 from django.urls import path, include
 from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+
+
+class LoginRequiredMixin(object):
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(LoginRequiredMixin, self).dispatch(*args, **kwargs)
+
+
+class LoginRequiredTemplateView(LoginRequiredMixin,TemplateView):
+    pass
 
 
 urlpatterns = [
     path('api/', include('training_logger.api.urls')),
-    path('', TemplateView.as_view(template_name='home.html')),
+    path('', LoginRequiredTemplateView.as_view(template_name='home.html')),
     path('login', TemplateView.as_view(template_name='login.html')),
     path('', include('social_django.urls', namespace='social'))
 ]
