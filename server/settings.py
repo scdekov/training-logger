9818a7989d2e9407050545ac8be584dbc805cf2e
svetoslav dekov <svetoslav.dekov@mentormate.com>
2020-01-12 10:38:17 +0200

    Require login for APIs

--- server/settings.py
+++ server/settings.py
@@ -130,7 +130,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
     ],
-    'DEFAULT_PERMISSION_CLASSES': [],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated'
+    ],
 }
 
 if ENVIRONMENT == 'production':
