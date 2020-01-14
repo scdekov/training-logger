11971682e6922cebf3c87b9faf4ef289a0c56a3c
svetoslav dekov <svetoslav.dekov@mentormate.com>
2020-01-14 16:13:22 +0200

    Add padding to x axis in weight chart

--- static/js/charts.jsx
+++ static/js/charts.jsx
@@ -19,14 +19,14 @@ class LineChart extends React.Component {
 
     render() {
         return (
-            <Recharts.LineChart width={400} height={400} data={this.state.data}>
+            <Recharts.LineChart width={800} height={400} data={this.state.data}>
                 <Recharts.Line connectNulls
                                type="monotone"
                                dataKey="weight"
                                stroke="#8884d8" />
                 <Recharts.CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
                 <Recharts.Tooltip />
-                <Recharts.XAxis dataKey="date"/>
+                <Recharts.XAxis dataKey="date" padding={{ left: 30, right: 30 }}/>
                 <Recharts.YAxis />
             </Recharts.LineChart>
         );
